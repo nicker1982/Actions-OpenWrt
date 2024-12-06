9ed984df0e52d2228750c66cd2cd1b3a3b9df8a0
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2024-12-06 08:28:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '1i src-git kenzok https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
+echo src-git kenzok https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo src-git small https://github.com/kenzok8/small' >>feeds.conf.default
