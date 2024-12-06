3742a3cb1674f566f48f369eaef0272be42c0467
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2024-12-06 10:38:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,4 +24,4 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
