348436cc73ef94dbcff5cbedb83d12bdfc6e6d3d
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2024-12-06 20:23:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
@@ -23,5 +28,4 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
