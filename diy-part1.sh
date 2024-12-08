a17065589d51f82478bf25954348ac3fc2bff6ec
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2024-12-08 11:04:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,12 +15,12 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # 自定义第三方插件
-git clone https://github.com/ximiTech/luci-app-msd_lite.git package/lean/luci-app-msd_lite
-git clone https://github.com/DongyangHu/luci-app-poweroff.git package/lean/luci-app-poweroff
-git clone https://github.com/neophack/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
-git clone https://github.com/chenmozhijin/luci-app-socat.git package/lean/luci-app-socat
+#git clone https://github.com/ximiTech/luci-app-msd_lite.git package/lean/luci-app-msd_lite
+#git clone https://github.com/DongyangHu/luci-app-poweroff.git package/lean/luci-app-poweroff
+#git clone https://github.com/neophack/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
+#git clone https://github.com/chenmozhijin/luci-app-socat.git package/lean/luci-app-socat
