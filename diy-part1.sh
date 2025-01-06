2a8e12fd620dee3731174d89efb431ae458f7f45
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-01-06 13:52:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,4 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 #git clone https://github.com/neophack/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
 #git clone https://github.com/chenmozhijin/luci-app-socat.git package/lean/luci-app-socat
 git clone https://github.com/momokind/luci-app-hypermodem.git package/lean/luci-app-hypermodem
-git clone https://github.com/Siriling/5G-Modem-Support.git package/lean/5G-Modem-Support/luci-app-modem
+git clone https://github.com/qianlyun123/luci-app-modem.git package/lean/luci-app-modem
