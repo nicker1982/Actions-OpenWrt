d3ce111b3c12707653ab26f5cfe1a11260233c4e
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-04-03 07:53:06 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -16,5 +16,5 @@
 
 # 自定义第三方插件
 git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
-git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
-git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+#git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
+#git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
