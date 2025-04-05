3bb5ed9c13baa7e776fa3ad6d8c635755e303d81
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-04-05 14:21:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -11,7 +11,8 @@
 #
 
 ### 添加第三方订阅源
-#sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 
 # 自定义第三方插件
