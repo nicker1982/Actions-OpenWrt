fb21083f60eb84eab1d776d28c737a9c01a2686a
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-04-13 14:47:54 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -18,5 +18,5 @@ echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.
 # 自定义第三方插件
 git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
 #git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
-#git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
 #git clone https://github.com/linkease/istore.git package/istore
