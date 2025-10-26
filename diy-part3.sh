d261a2c18f5e27b59ff6a41e7854f22cd23fa97b
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-10-26 13:28:39 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -11,12 +11,13 @@
 #
 
 ### 添加第三方订阅源
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
 
 # 自定义第三方插件
 git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
 #git clone https://github.com/qianlyun123/luci-app-modem.git package/luci-app-modem
-git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
+#git clone https://github.com/Siriling/5G-Modem-Support.git package/5G-Modem-Support
 #git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
